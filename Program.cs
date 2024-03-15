a790c8bced694405b2c61471d9994f081087a535
mszym99 <Matthew.Szymanski@IntelliTect.com>
2024-03-15 16:49:12 -0700

    initial commit kernel setup

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
+using Barracuda2.Helpers;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Memory;
+string? endpoint = "endpoint";
+string? modelId = "gpt-3.5-turbo";
+string? embeddingModel = "text-embedding-3-small";
+string? apiKey = "";
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Configuration.AddJsonFile("appsettings.json").AddEnvironmentVariables();
+IMemoryStore store = MemoryStore.CreateSamplePostgresMemoryStore();
+var kernel = Kernel.CreateBuilder()
+            .AddOpenAIChatCompletion(modelId, apiKey)
+            .AddOpenAITextEmbeddingGeneration(embeddingModel, apiKey)
+            .Build();
+// Create an embedding generator to use for semantic memory.
+var embeddingGenerator = new OpenAITextEmbeddingGenerationService(embeddingModel, apiKey);
+
+// The combination of the text embedding generator and the memory store makes up the 'SemanticTextMemory' object used to
+// store and retrieve memories.
+SemanticTextMemory textMemory = new(store, embeddingGenerator);
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
